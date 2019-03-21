ff71609ed3caaacdadcd4191e7fa35c2e9f9cce4
Kenneth Salanga <ksalanga@ucvts.tec.nj.us>
2019-03-21 14:26:56 -0400

    format

--- src/Main.java
+++ src/Main.java
@@ -7,27 +7,28 @@ public class Main {
 		 
 		 Card[] deck = new Card[52];
 		    
-			int x = 0;
+		int x = 0;
 			 
-			for(Suit suit : suits) {
-			     for(Rank rank : ranks) {
-			        deck[x] = new Card(suit, rank);
-			        x++;
-			     }
-			 }
-			
-			//shuffle (algorithm taken from Princeton.edu)
-			for (int i = 0; i < deck.length; i++) {
-	            int r = i + (int) (Math.random() * (deck.length-i));
-	            Card temp = deck[r];
-	            deck[r] = deck[i];
-	            deck[i] = temp;
-	        }
-			
-			for (int i = 0; i < deck.length; i++) {
-				System.out.println(deck[i].toString());
-			}
-			
+		for(Suit suit : suits) {
+		     for(Rank rank : ranks) {
+		        deck[x] = new Card(suit, rank);
+		        x++;
+		     }
+		 }
+		
+		//shuffle (algorithm taken from Princeton.edu)
+		for (int i = 0; i < deck.length; i++) {
+            int r = i + (int) (Math.random() * (deck.length-i));
+            Card temp = deck[r];
+            deck[r] = deck[i];
+            deck[i] = temp;
+        }
+		
+		for (int i = 0; i < deck.length; i++) {
+			System.out.println(deck[i].toString());
+		}
+		
+		//.valueOf()
 			
 	}
 }
